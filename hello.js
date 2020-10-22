837b5b9210a2ee6b1d053a9097926fdad3c469d7
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-10-22 10:00:23 -0400

    [JS] First 5 hello

---
+++ hello.js
@@ -0,0 +1,33 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' . How are you?';
+    return msg;
+
+}
+console.log(sayHello('SEI 1019'));
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' . How are you?';
+    return msg;
+}
+
+console.log(sayHello('Tu Anh Huynh'));
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' . How are you?';
+    return msg;
+}
+console.log(sayHello('SEI Day 4'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + ' . How are you?';
+    return msg;
+}
+console.log(sayHello('Rome'));
+
+function sayHello(name) {
+    let msg = 'hello, ' + name + ' . How are you?';
+    return msg;
+}
+console.log(sayHello("class"))

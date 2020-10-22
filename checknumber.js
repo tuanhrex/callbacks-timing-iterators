089b2274ed777837e6093f8fff647fb15b9363d0
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-10-22 10:15:51 -0400

    [New JS] Added checknumber.js and repeated function 5 times

---
+++ checknumber.js
@@ -0,0 +1,54 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0)  {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(5))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(4))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num <0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(0))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if ( num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(-10))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if ( num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(-90))
